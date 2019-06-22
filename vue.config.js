32540f94fbfdcaa381d25399dfc8e2786b27a6f1
Andrey Moroz <drew.vfrost@gmail.com>
2019-06-22 14:42:39 +0300

    Playing audio on key pushes

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   devServer: {
     // can be replaced on cmd with `vue serve --host dev2.kaido.team`
-    host: 'dev2.kaido.team'
+    // host: 'dev2.kaido.team'
+    host: 'localhost'
   }
 }
